122a99e181142c19acd5ca35082f5975a542a605
Quoocscuongwf <lequoccuong2204@gmail.com>
2024-12-07 16:12:51 +0700

    j

--- admin/assets/js/admin.js
+++ admin/assets/js/admin.js
@@ -2,6 +2,6 @@ var editForm = document.getElementById('edit-form')
 editForm.style.display='none'
 const editBook = document.getElementById('edit-book');
 editBook.addEventListener('click', ()=>{
-    editForm.style.position="fixed"
+    editForm.style.position="absolute"
     editForm.style.display='block'
 })
